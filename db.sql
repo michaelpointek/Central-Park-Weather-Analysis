c8e4ad71d8f1e9ba4c8a4d5626dce363ac153923
michaelpointek <michaelpointek@gmail.com>
2024-04-17 14:51:13 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,17 @@
-ALTER USER postgres WITH PASSWORD 'postgres';
+create table ny_weather_data_set (
+	id date PRIMARY KEY,
+	prcp float,
+	snow float,
+	sndw float,
+	tmin INT,
+	tmax INT
+);
+
+select *
+from ny_weather_data_set;
+
+delete from ny_weather_data_set
+where sndw IS NULL;
+
+alter table ny_weather_data_set
+rename sndw to snwd;
