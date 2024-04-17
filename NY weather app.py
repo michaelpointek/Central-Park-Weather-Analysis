82125831b493b9d481592e8215319b47fe9e7b2d
Jordan Mayville <154078445+jmayville04@users.noreply.github.com>
2024-04-16 21:59:21 -0400

    updated files

--- NY weather app.py
+++ NY weather app.py
@@ -1,11 +1,14 @@
 # Import the dependencies.
 # import datetime as dt
 
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, MetaData, text
-
 from flask import Flask, jsonify, render_template
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+from flask_cors import CORS, cross_origin
+import pandas as pandas
+import numpy as numpy
+from config import user, password
 
 
 
@@ -45,17 +48,14 @@ from flask import Flask, jsonify, render_template
 
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 # Database Setup
-connection_string = "postgresql://postgres:postgres@localhost:5432/Project 3"
+connection_string = f"postgresql://{user}:{password}@localhost:5432/Project 3"
 engine = create_engine(connection_string)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 NyWeatherDataSet = Base.classes.ny_weather_data_set
-# Flask Routes
-# @app.route('/')
-# def home():
-#     # Render the HTML template
-#     return render_template('html.html')
+
 @app.route('/')
 def get_data():
     # Create a session to query the database
@@ -78,6 +78,30 @@ def get_data():
         })
     # Return the data as JSON
     return jsonify(data)
+
+@app.route("/api/yearly-average")
+@cross_origin(supports_credentials=True)
+def get_data2():
+    # Create a session to query the database
+    session = Session(engine)
+    # Query the ny_weather_data_set and fetch the data
+    results = session.query(NyWeatherDataSet.date, NyWeatherDataSet.prcp, NyWeatherDataSet.snow,
+                            NyWeatherDataSet.snwd, NyWeatherDataSet.tmin, NyWeatherDataSet.tmax).all()
+    # Close the session
+    session.close()
+    # Convert the results to a list of dictionaries
+    data = []
+    for result in results:
+        data.append({
+            "Date": result.date,
+            "Precipitation": result.prcp,
+            "Snow": result.snow,
+            "SNWD": result.snwd,
+            "Minimum Temperature": result.tmin,
+            "Maximum Temperature": result.tmax,
+        })
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(debug=False)
 
