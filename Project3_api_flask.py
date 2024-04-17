8a404d808f1d3721a050f79089d798da333bdb7f
wmidson <wmidson@icloud.com>
2024-04-16 21:40:18 -0400

    updates

--- Project3_api_flask.py
+++ Project3_api_flask.py
@@ -1,20 +1,23 @@
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify 
+from flask_cors import CORS, cross_origin
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
+from config import user, password
 
 # Flask Setup
 app = Flask(__name__)
-
+CORS(app, supports_credentials=True)
 # Database Setup
-connection_string = "postgresql://postgres:postgres@localhost:5432/project3"
+connection_string = f"postgresql://{user}:{password}@localhost:5432/project3"
 engine = create_engine(connection_string)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 NyWeatherDataSet = Base.classes.ny_weather_data_set
 
 @app.route('/')
+@cross_origin(supports_credentials=True)
 def get_data():
     # Create a session to query the database
     session = Session(engine)
