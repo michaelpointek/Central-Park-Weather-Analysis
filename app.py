de79bebb35f9739d1788a9d93a269f9116973a2e
michaelpointek <michaelpointek@gmail.com>
2024-04-16 21:32:32 -0400

    Mike's commits 1

--- app.py
+++ app.py
@@ -1,23 +1,68 @@
-# Import the dependencies.
-# import datetime as dt
-from sqlalchemy.ext.automap import automap_base
+from flask import Flask, jsonify, render_template
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, MetaData, text
-from flask import Flask, jsonify
-#################################################
+from sqlalchemy.ext.automap import automap_base
+from flask_cors import CORS, cross_origin
+import pandas as pd
+import numpy as np
+
+# Flask Setup
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
 # Database Setup
-#################################################
-#create postgres connection
 connection_string = "postgresql://postgres:postgres@localhost:5432/project_3"
-#create engine
-engine = create_engine(connection_string).connect()
-# reflect an existing database into a new model
+engine = create_engine(connection_string)
 Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
-# Create our session (link) from Python to the DB
-session = Session(engine)
-query = text("select * from ny_weather_data_base")
-data = conn.execute(query)
-for i in data:
-    print(i)
+Base.prepare(engine, reflect=True)
+NyWeatherDataSet = Base.classes.ny_weather_data_set
+
+############## ROUTES ################
+@app.route('/')
+@cross_origin(supports_credentials=True)
+def get_data():
+    # Create a session to query the database
+    session = Session(engine)
+    # Query the ny_weather_data_set and fetch the data
+    results = session.query(NyWeatherDataSet.date, NyWeatherDataSet.prcp, NyWeatherDataSet.snow,
+                            NyWeatherDataSet.snwd, NyWeatherDataSet.tmin, NyWeatherDataSet.tmax).all()
+    # Close the session
+    session.close()
+    # Convert the results to a list of dictionaries
+    data = []
+    for result in results:
+        data.append({
+            "Date": result.date,
+            "Precipitation": result.prcp,
+            "Snow": result.snow,
+            "SNWD": result.snwd,
+            "Minimum Temperature": result.tmin,
+            "Maximum Temperature": result.tmax,
+        })
+    return jsonify(data)
+
+@app.route('/api/yearly-average')
+@cross_origin(supports_credentials=True)
+def get_yearly_average():
+    # Create a session to query the database
+    session = Session(engine)
+    # Query the ny_weather_data_set and fetch the data
+    results = session.query(NyWeatherDataSet.date, NyWeatherDataSet.prcp, NyWeatherDataSet.snow,
+                            NyWeatherDataSet.snwd, NyWeatherDataSet.tmin, NyWeatherDataSet.tmax).all()
+    # Close the session
+    session.close()
+    # Convert the results to a list of dictionaries
+    data = []
+    for result in results:
+        data.append({
+            "Date": result.date,
+            "Precipitation": result.prcp,
+            "Snow": result.snow,
+            "SNWD": result.snwd,
+            "Minimum Temperature": result.tmin,
+            "Maximum Temperature": result.tmax,
+        })
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
