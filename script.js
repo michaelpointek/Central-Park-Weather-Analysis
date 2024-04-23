9ee7174d3edbb393db629c70143f052c17b642da
Jordan Mayville <154078445+jmayville04@users.noreply.github.com>
2024-04-22 21:20:39 -0400

    updated .js file

--- script.js
+++ script.js
@@ -5,34 +5,3 @@ const dataURL = "http://127.0.0.1:5000/"
 d3.json(dataURL).then(function(dataURL) {
     console.log(dataURL)
 });
-
-// Function to fetch historical data by month
-function getHistoricalDataByMonth(month, year) {
-    fetch(`http://localhost:5000/historical-data-by-month?month=${month}&year=${year}`)
-        .then(response => response.json())
-        .then(dataURL => {
-            // Process and display historical data
-            console.log(dataURL);
-        })
-        .catch(error => console.error('Error fetching historical data:', error));
-}
-
-// Function to filter data by temperature range
-function filterData(minTemp, maxTemp) {
-    fetch(`http://localhost:5000/data-filtering?min_temp=${minTemp}&max_temp=${maxTemp}`)
-        .then(response => response.json())
-        .then(dataURL => {
-            // Process and display filtered data
-            console.log(dataURL);
-        })
-        .catch(error => console.error('Error filtering data:', error));
-}
-
-// Function to generate bar graph visualization
-fetch("http://localhost:5000/bar-graph")
-    .then(response => response.json())
-    .then(dataURL => {
-        // Process and display bar graph
-        console.log(dataURL);
-    })
-    .catch(error => console.error('Error generating bar graph:', error));
